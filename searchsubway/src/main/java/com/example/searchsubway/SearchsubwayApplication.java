7634cc3ceb1867632771da67dd312d009e385453
남궁찬 <81404890+Chan531@users.noreply.github.com>
2022-08-14 16:08:35 +0900

    Add files via upload

---
+++ searchsubway/src/main/java/com/example/searchsubway/SearchsubwayApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchsubway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchsubwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchsubwayApplication.class, args);
+	}
+
+}
